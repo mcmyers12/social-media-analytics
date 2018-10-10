80ed16f547d57ace37adf20472ca59a08c29604f
mcmyers12 <mirandacmyers12@gmail.com>
2018-10-10 00:35:10 -0400

    Finalized machine learning assignment

--- projects/machine-learning/facebook-impressions.py
+++ projects/machine-learning/facebook-impressions.py
@@ -21,10 +21,7 @@ def getTrainTest(fileName):
 
 def getFeatures(df):
     return {
-        'postMessage': np.array(list(df['Post Message'])),
-        'type': np.array(list(df['Type'])),
-        'weather': np.array(list(df['Weather'])),
-        'weekend': np.array(list(df['Weekend']))
+        'postMessage': np.array(list(df['Post Message']))
     }
 
 
@@ -40,21 +37,14 @@ def runClassifier(fileName):
     test_features = getFeatures(test_df)
     test_labels = getLabels(test_df)
 
-    # Training input on the whole training set with no limit on training epochs.
     train_input_fn = tf.estimator.inputs.numpy_input_fn(train_features, train_labels, num_epochs=None, shuffle=True)
 
-    # Prediction on the whole training set.
+    # Prediction on the training set.
     predict_train_input_fn = tf.estimator.inputs.numpy_input_fn(
         train_features, train_labels, shuffle=False)
     # Prediction on the test set.
     predict_test_input_fn = tf.estimator.inputs.numpy_input_fn(
         test_features, test_labels, shuffle=False)
-
-
-    '''vocabulary_feature_column =
-        tf.feature_column.categorical_column_with_vocabulary_list(
-            key='type',
-            vocabulary_list=["kitchenware", "electronics", "sports"])'''
 
     embedded_text_feature_column = hub.text_embedding_column("postMessage", "https://tfhub.dev/google/nnlm-en-dim128/1")
     feature_columns = [embedded_text_feature_column]
@@ -65,9 +55,7 @@ def runClassifier(fileName):
         n_classes=2,
         optimizer=tf.train.AdagradOptimizer(learning_rate=0.003))
 
-
     estimator.train(input_fn=train_input_fn, steps=1000);
-
 
     train_eval_result = estimator.evaluate(input_fn=predict_train_input_fn)
     test_eval_result = estimator.evaluate(input_fn=predict_test_input_fn)
@@ -75,6 +63,28 @@ def runClassifier(fileName):
     print("Training set accuracy: {accuracy}".format(**train_eval_result))
     print("Test set accuracy: {accuracy}".format(**test_eval_result))
 
+    def get_predictions(estimator, input_fn):
+        return [x["class_ids"][0] for x in estimator.predict(input_fn=input_fn)]
+
+    # Create a confusion matrix on training data.
+    with tf.Graph().as_default():
+        cm = tf.confusion_matrix(test_labels,
+                                 get_predictions(estimator, predict_test_input_fn))
+        with tf.Session() as session:
+            cm_out = session.run(cm)
+
+    # Normalize the confusion matrix so that each row sums to 1.
+    cm_out = cm_out.astype(float) / cm_out.sum(axis=1)[:, np.newaxis]
+
+    # Precision and recall
+    TP = cm_out[1][1]
+    FP = cm_out[0][1]
+    FN = [1][0]
+    precision = TP / (TP + FP)
+    recall = TP / (TP + FN)
+
+    print 'precision: ' + str(precision)
+    print 'recall: ' + str(recall)
 
 
 runClassifier('data/SocialMediaInsightsforMachineLearning.xlsm')
